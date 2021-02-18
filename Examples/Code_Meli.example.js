60ea692df58bfa7a01f55705a5f26b50ab16b5ef
rozi <rozbeh24@outlook.com>
2021-02-18 19:10:06 +0330

    added Examples

---
+++ Examples/Code_Meli.example.js
@@ -0,0 +1,5 @@
+const { checkCodeMeli } = require('../Module/Code_Meli')
+
+console.log(checkCodeMeli('1234567890')); //worng Code Meli
+
+console.log(checkCodeMeli('0068842368')); //example correct Code Meli
